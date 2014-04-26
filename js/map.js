b0742e41de082f5790eb63487cb588ea11839046
Alex Jegtnes <saccosekk@gmail.com>
2014-04-26 16:46:05 +0100

    Adding automatic geolocation capability and custom marker for this

--- js/map.js
+++ js/map.js
@@ -5,5 +5,27 @@ $(document).ready(function() {
       lat: 51.499940,
       lng: -2.547272
     });
+
+    GMaps.geolocate({
+    success: function(position){
+      map.setCenter(position.coords.latitude, position.coords.longitude);
+
+      map.addMarker({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+        icon: "/build/img/map-marker-geolocation.png",
+        title: 'You are here.',
+        infoWindow: {
+          content: '<p>You are here!</p>'
+        }
+      });
+    },
+    error: function(error){
+      console.log('Geolocation failed: '+error.message);
+    },
+    not_supported: function(){
+      console.log("Your browser has no geolocation")
+    }
+  });
   }
 });
