74faf0625edf8e48704440c3b5308a957b033e8a
Alex Jegtnes <saccosekk@gmail.com>
2014-04-27 15:23:31 +0100

    Better toast notifications

--- js/cards.js
+++ js/cards.js
@@ -5,21 +5,20 @@ $(document).ready(function() {
     event.stopPropagation();
   });
 
-  $('.card__overlay__section--like,
-  .card__overlay__section--dislike').on('click', function() {
+  $('.card__overlay__section--like, .card__overlay__section--dislike').on('click', function() {
+    verb = $(this).hasClass('card__overlay__section--like') ? "Like" : "Dislike"
     rated = $(this).hasClass('js-rated');
     $(this).toggleClass('js-rated');
     $(this).toggleClass('card__overlay__social-stat--active');
-
     stat = $(this).find('.card__overlay__social-stat');
     rating = parseInt(stat.text());
     if (rated) {
       rating--;
-      var toast = new fries.Toast({ content: "Removed sentiment from content." });
+      var toast = new fries.Toast({ content: "Removed " + verb + " from this!" });
     }
     else {
       rating++;
-      var toast = new fries.Toast({ content: "Added sentiment to content." });
+      var toast = new fries.Toast({ content: verb + "d this!." });
     }
     stat.text(rating);
 
