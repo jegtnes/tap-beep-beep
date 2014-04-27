1f937a31596ad42596c026e45b681d5cd5baefd4
Alex Jegtnes <saccosekk@gmail.com>
2014-04-27 14:06:38 +0100

    Adding ability to 'like' or 'dislike' posts

--- js/cards.js
+++ js/cards.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
   }).find('.card__overlay__content').on('click', function(event) {
     event.stopPropagation();
   });
+
+  $('.card__overlay__section--like,
+  .card__overlay__section--dislike').on('click', function() {
+    stat = $(this).find('.card__overlay__social-stat');
+    rating = parseInt(stat.text());
+    newRating = rating + 1;
+    stat.text('' + newRating);
+  })
 });
