0ed8e86ccb4af4f32efedfc1ad907d30c384fe62
Mark J Smith <mark@markjs.net>
2014-04-28 00:49:12 +0100

    Upload photo with other element other than file input

--- js/misc.js
+++ js/misc.js
@@ -2,5 +2,10 @@ $(document).ready(function() {
   $('#tapInToBegin, #tapInExplanation').on('click', function() {
     $('#tapInExplanation').toggleClass('explanation-hidden')
   });
+
   $('.action-overflow-hidden').removeClass('action-overflow-hidden');
+
+  $('#file-button').on('click', function() {
+    $('input[type=file]')[0].click();
+  });
 });
