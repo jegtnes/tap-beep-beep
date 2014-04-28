4fbf86fb39492c58f064b3e31d14b238cf7b4e61
Mark J Smith <mark@markjs.net>
2014-04-28 01:14:05 +0100

    Whitespace

--- js/map.js
+++ js/map.js
@@ -98,25 +98,25 @@ $(document).ready(function() {
     // Kudos to the the fine ladies and gents at Paulund for this function
     // http://www.paulund.co.uk/create-google-maps-with-gmaps-js
     GMaps.geolocate({
-    success: function(position){
-      map.setCenter(position.coords.latitude, position.coords.longitude);
+      success: function(position){
+        map.setCenter(position.coords.latitude, position.coords.longitude);
 
-      map.addMarker({
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-        icon: "/build/img/map-marker-geolocation.png",
-        title: 'You are here.',
-        infoWindow: {
-          content: '<p>You are here!</p>'
-        }
-      });
-    },
-    error: function(error){
-      console.log('Geolocation failed: '+error.message);
-    },
-    not_supported: function(){
-      console.log("Your browser has no geolocation")
-    }
-  });
+        map.addMarker({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+          icon: "/build/img/map-marker-geolocation.png",
+          title: 'You are here.',
+          infoWindow: {
+            content: '<p>You are here!</p>'
+          }
+        });
+      },
+      error: function(error){
+        console.log('Geolocation failed: '+error.message);
+      },
+      not_supported: function(){
+        console.log("Your browser has no geolocation")
+      }
+    });
   }
 });
